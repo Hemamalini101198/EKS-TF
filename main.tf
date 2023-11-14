cd76cfffba63a8728144c584fc45f7e707621af9
Hemamalini101198 <150349833+Hemamalini101198@users.noreply.github.com>
2023-11-14 13:09:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ module "eks_cluster" {
   source          = "./Modules/EKS-cluster"
   cluster_name    = "var.cluster_name"
   subnets         = "var.private_subnets"
-  lable_name = general
+  #lable_name = general
   managed_node_desired_size = var.managed_node_desired_size
   managed_node_min_size     = var.managed_node_min_size
   managed_node_max_size     = var.managed_node_max_size
