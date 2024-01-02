f94b239dcc4342271c00ae29d6eeae9d2dc00095
Hemamalini101198 <150349833+Hemamalini101198@users.noreply.github.com>
2024-01-02 15:51:23 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -141,7 +141,7 @@ resource "aws_security_group" "sg" {
     from_port   = 0
     to_port     = 65535
     protocol    = "tcp"
-    cidr_blocks = var.cidr_block
+    cidr_blocks = ["0.0.0.0/0"]
   } 
 
   egress {
